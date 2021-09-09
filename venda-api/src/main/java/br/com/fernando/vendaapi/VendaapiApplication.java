8c40402015b258c51b1821564fa4b73acd13f70a
FernandoGonsales <fert994gonsales@gmail.com>
2021-09-09 20:12:55 -0300

    Criando API com Spring Initialzr

---
+++ venda-api/src/main/java/br/com/fernando/vendaapi/VendaapiApplication.java
@@ -0,0 +1,14 @@
+package br.com.fernando.vendaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaapiApplication.class, args);
+		String a;
+	}
+
+}
---
+++ venda-api/src/test/java/br/com/fernando/vendaapi/VendaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fernando.vendaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
