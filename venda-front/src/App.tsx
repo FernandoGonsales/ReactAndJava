1adb98607d73f76d58084c121a0b4fd2a72350ff
FernandoGonsales <fert994gonsales@gmail.com>
2021-09-12 17:57:57 -0300

    static charts

--- venda-front/src/App.tsx
+++ venda-front/src/App.tsx
@@ -1,16 +1,33 @@
+import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
 function App() {
   return (
     <>
-      <NavBar/>
+      <NavBar />
       <div className="container">
-        <h1 className="text-primary">Olá Mundo</h1>
-        <DataTable/>
+        <h1 className="text-primary py-3">Painel de Vendas</h1>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary" title="Calcula a porcentagem de negócios fechados com base no total de clientes visitados.">
+              Taxa de vendas (%)
+            </h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas as Vendas</h5>
+            <DonutChart />
+          </div>
+        </div>
+        <div className="py-5">
+          <h2 className="text-primary">Histórico de Vendas Realizadas</h2>
+        </div>
+        <DataTable />
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 }
