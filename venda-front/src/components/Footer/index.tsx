5f482739d05c003d8456d61c6a6055bbd37e95a4
FernandoGonsales <fert994gonsales@gmail.com>
2021-09-12 17:29:57 -0300

    Basic static components

---
+++ venda-front/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto bg-dark">
+      <div className="container">
+        <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/fernando-fgonsales/" target="_blank" rel="noreferrer">Fernando Gonsales</a></p>
+        <p className="text-light"><small><strong>Java Spring / React</strong><br />
+          Promovido pela DevSuperior, conferem lá: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
